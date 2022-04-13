b0e894aed5740c6afd6d8dad9ba78f7d5ebfbf7f
root <root@localhost.localdomain>
2022-04-13 18:22:02 +0800

    int springboot project

---
+++ backend/tradingSystem/src/main/java/com/example/tradingSystem/TradingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.tradingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingSystemApplication.class, args);
+	}
+
+}
