b9bd3a670a38eae5580f7cbc74f7fa13316860d5
shen-wenxin <shen_wenxin@qq.com>
2022-04-14 16:00:35 +0800

    fix springboot problem

--- backend/tradingSystem/src/test/java/com/example/demo/DemoApplicationTests.java
+++ backend/tradingSystem/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.tradingSystem;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TradingSystemApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
